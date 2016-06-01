26a84f7cf77314abac6d4cdc7ba8f6a57efb9d76
Sarekoski Ukko <ukonvaaja10@gmail.com>
2016-06-01 13:07:48 +0300

    Tunkkibot listens and sends request to api

--- tunkki-bot/bot.js
+++ tunkki-bot/bot.js
@@ -1,4 +1,26 @@
 var Botkit = require('botkit');
+var request = require('request');
+
+function send_to_infoscreen(text) {
+  var success = false;
+  var options = {
+    method: 'PATCH',
+    uri: 'localhost:3000/componentdata'
+  };
+
+  request.post(
+    options,
+    {form: {name:'SlackBot'}, data: {current_message: text}},
+    function(err, httpResponse, body) {
+     if (err) {
+       status = false;
+     } else {
+       status = true;
+     }
+   });
+
+   return status;
+}
 
 var controller = Botkit.slackbot({
   debug: false
@@ -6,8 +28,11 @@ var controller = Botkit.slackbot({
 
 controller.spawn({
   token: "xoxb-36601590166-ShtScvJ1xcKk7FjklxQ7ugrd",
-}).startRTM()
+}).startRTM();
 
-controller.hears('tunkkitesti',['direct_message','direct_mention','mention'],function(bot,message) {
-  bot.say({text:"test", channel: "#tunkkidemo"});
+controller.hears(['!info'],'ambient',function(bot,message) {
+  var text = message.text.slice(6);
+  var success = send_to_infoscreen(text);
+  var reply = success ? "Message sent." : "Sending failed.";
+  bot.reply(message, reply);
 });
